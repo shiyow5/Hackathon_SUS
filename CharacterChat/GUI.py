31b2b0a04d16d09b15286a7f0c047e5d1318b6d9
shiyow5 <s1300221@u-aizu.ac.jp>
2025-02-16 11:31:08 +0900

    implement about voicevox

--- CharacterChat/GUI.py
+++ CharacterChat/GUI.py
@@ -8,6 +8,7 @@ from CharacterChat.my_lib.search_character import CharacterFeature
 from CharacterChat.my_lib.fine_tuning import CharacterTuning
 from CharacterChat.my_lib.fine_tuning import save_serifu_data
 from CharacterChat.my_lib.fine_tuning import create_model
+from CharacterChat.my_lib.voicevox import VoiceVox
 
 
 # [0]は表示名
@@ -134,6 +135,10 @@ def chat(name = "ずんだもん", model_name = "zundamon1"):
         # チャットメッセージとして表示
         with st.chat_message("assistant"):
             st.markdown(response)
+        
+        # ずんだもんにしゃべらせる
+        vv = VoiceVox()
+        vv.speak(response)
 
 @st.dialog("本当に削除しますか？")
 def delete(model):
